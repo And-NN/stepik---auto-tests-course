dc4b9595fb3ff289fd8d3fcfd1ad461dced28610
And-NN <andrew.nnma@gmail.com>
2020-04-25 13:14:29 +0300

    ассерты и составные сообщения

---
+++ ассерты и составные сообщения.py
@@ -0,0 +1,23 @@
+print("Let's count together: {}, then goes {}, and then {}".format("one", "two", "three"))
+
+str1 = "one"
+str2 = "two"
+str3 = "three"
+print(f"Let's count together: {str1}, then goes {str2}, and then {str3}")
+
+actual_result = "abrakadabra"
+f"Wrong text, got {actual_result}, something wrong"
+
+print(f"{2+3}")
+
+assert abs(-42) == 42, "Should be absolute value of a number"
+
+
+s = 'My Name is Julia'
+
+if 'Name' in s:
+    print('Substring found')
+
+index = s.find('Name')
+if index != -1:
+    print(f'Substring found at index {index}')
